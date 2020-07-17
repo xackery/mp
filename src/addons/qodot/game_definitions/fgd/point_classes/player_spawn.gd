7554eb1e0eb8f5977992c082c69a92116c6698d2
Ali Al-Khalifa <am.alkhalifa@gmail.com>
2020-07-17 13:28:09 +0300

    Integration with trenchbroom completed
    * Zoo affordances made
    * Spawner example
    * entity example

---
+++ src/addons/qodot/game_definitions/fgd/point_classes/player_spawn.gd
@@ -0,0 +1,22 @@
+extends QodotEntity
+
+var player: Spatial
+var spawn_facing := 0.0
+
+func update_properties():
+	.update_properties()
+	if 'angle' in properties:
+		spawn_facing = deg2rad(properties.angle.to_float())
+
+func _ready():
+	call_deferred("_spawn")
+
+func _spawn():
+	player = preload("res://entities/player/player.tscn").instance()
+	var active_scene = get_node("/root/main").active_scene
+	print(active_scene)
+	var scene = get_node("/root/main").get_node(active_scene)
+	scene.add_child(player)
+	player.owner = scene
+	player.get_node("Components/PlayerMovementController").rotation_y = spawn_facing
+	player.translation = translation
--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,7 +1,12 @@
 extends Control
 
+export var active_scene: NodePath
+
 func _ready():
-	pass # Replace with function body.
+	var screen_size = OS.get_screen_size() - Vector2(0, 64)
+	OS.window_size.y = screen_size.y * 0.9
+	OS.window_size.x = OS.window_size.y * 4/3
+	OS.window_position = screen_size / 2 - OS.window_size / 2
 
 func _input(event):
 	$Viewport.input(event)
